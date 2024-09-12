b4bd26f6dd67dd7ed0183bf007e36ae577c7b1f6
sineptic <sineptic0@gmail.com>
2024-09-12 09:08:14 +0300

    add expamples

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use s_text_input_f_parser::parse_paragraph;
 
-fn main() {
-    let input = "hello `world`!";
+fn test(input: &str) {
     let output = parse_paragraph(input);
+    println!("------------------------------------------------------------------");
+    println!("input: {input}");
     println!("{output:#?}");
+    println!();
+}
+
+fn main() {
+    test("hello `world`!");
+
+    test("`precipice` - a very steep side of a cliff or a mountain");
+    test("precipice - a very `steep` side of a `cliff` or a `mountain`");
+
+    test("`in advance` - before a particular time, or before doing a particular thing");
+    test("in advance - `before` a particular time, or `before` doing a particular thing");
+
+    test("`consequence` - a result of a particular action or situation, often one that is bad or not convenient");
+    test("consequence - a `result` of a particular action or situation, often one that is `bad` or `not convenient`");
 }
